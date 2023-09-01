d98dbcc2f776d70cd254217d8c44225a139663f7
Chaïner KOMISSA ZOTSU <essikomissa@gmail.com>
2023-09-01 08:47:32 +0000

    Commit 15
    Login et upload de fichiers fonctionnels; reste l'affichage

--- ShareHub/main.py
+++ ShareHub/main.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request, render_template, redirect, url_for, flash
 import mysql.connector
+from datetime import datetime
+from werkzeug.utils import secure_filename, os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_key'
@@ -37,35 +39,37 @@ def login():
             flash('Identifiants incorrects, Veuillez réessayer', category='error')
     return render_template('login.html')
 
+
 @app.route('/logout')
 def logout():
     return render_template('home.html')
 
 
 ##CREATE
-@app.route('/upload')
-def upload():
-    '''if request.method == 'POST':
+@app.route('/upload/<string:user_id>', methods=['GET', 'POST'])
+def upload(user_id):
+    if request.method == 'POST':
         nom = request.form['namefile']
         description = request.form['description']
         date = datetime.now()
         added = date.strftime('%d-%m-%Y %H:%M:%S')
-        file = request.files['file']
+        fichier = request.files.get('fichier')
 
-        filename = secure_filename(file.filename)
-        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(image_path)
+        if fichier is not None and fichier.filename != '':
+            filename = secure_filename(fichier.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            fichier.save(image_path)
 
-        cursor = db.cursor()
-        query = "INSERT INTO File (namefile, description, linkfile, added) " \
-                "VALUES (%s, %s, %s, %s)"
-        cursor.execute(query, (nom, description, image_path, added))
-        db.commit()
-        cursor.close()
-        flash('Fichier déposé avec succès!', category='success')
-        return redirect(url_for('views.files'))
-    else :
-        flash('Nous avons rencontré une erreur!', category='error')'''
+            cursor = db.cursor()
+            query = "INSERT INTO File (namefile, description, linkfile, added, user_id) " \
+                    "VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(query, (nom, description, image_path, added, user_id))
+            db.commit()
+            cursor.close()
+            flash('Fichier déposé avec succès!', category='success')
+            return render_template('files.html')
+        else :
+            flash('Nous avons rencontré une erreur!', category='error')
     return render_template('upload.html')
 
 
